b38a6085440af08da33b5d29bd01b582b21485ca
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2024-06-08 14:14:40 +0300

    1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,19 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB, DB_DATA } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'node:fs/promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  if (typeof number === 'number' && number > 0) {
+    try {
+      const contacts = [...DB_DATA];
+      for (let i = 0; i < number; i++) {
+        contacts.push(createFakeContact());
+      }
+      fs.writeFile(PATH_DB, JSON.stringify(contacts));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
 
 await generateContacts(5);
